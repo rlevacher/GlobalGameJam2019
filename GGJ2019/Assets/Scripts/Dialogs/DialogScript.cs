55fa6abf9b345e0dec8e4a312720868fe88ead3b
PierreBio <45881846+PierreBio@users.noreply.github.com>
2019-01-26 14:30:46 +0100

    add dialog script in SampleScene

---
+++ GGJ2019/Assets/Scripts/Dialogs/DialogScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogScript : MonoBehaviour
+{
+    public TextMeshProUGUI textDisplay;
+    public string[] sentences;
+    private int index;
+    public float typingSpeed;
+
+    public GameObject continueButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Type());
+    }
+
+    IEnumerator Type()
+    {
+        continueButton.SetActive(false);
+
+
+        foreach (char letter in sentences[index].ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    public void NexSentence()
+    {
+        if (index < sentences.Length - 1)
+        {
+            index++;
+            textDisplay.text = "";
+            StartCoroutine(Type());
+        }
+        else
+        {
+            textDisplay.text = "";
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (textDisplay.text ==sentences[index])
+        {
+            continueButton.SetActive(true);
+        }
+        else
+        {
+            continueButton.SetActive(false);
+        }
+    }
+}
