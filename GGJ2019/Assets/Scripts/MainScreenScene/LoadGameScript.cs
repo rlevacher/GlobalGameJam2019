41484e20bf8b74034f522462a6557baa207d0ef8
PierreBio <45881846+PierreBio@users.noreply.github.com>
2019-01-26 13:06:30 +0100

    Add MainScreenScene

---
+++ GGJ2019/Assets/Scripts/MainScreenScene/LoadGameScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class LoadGameScript : MonoBehaviour
+{
+        public Button playButton;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playButton.onClick.AddListener(loadGame);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void loadGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
